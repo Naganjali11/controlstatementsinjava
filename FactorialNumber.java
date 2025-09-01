be053925e50adce3a979b2c9918c37aa922c747d
unknown <naganjali425@gmail.com>
2025-09-01 22:04:29 +0530

    javafullstackprograms

---
+++ FactorialNumber.java
@@ -0,0 +1,19 @@
+package com.codegnan.controlstatements;
+import java.util.Scanner;
+public class FactorialNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter a Number:");
+		int number=scanner.nextInt();
+		long result=1;
+		for(int i=1;i<=number;i++) {
+			result*=i;
+		}
+		System.out.println("Factorial of" +number+ "is:" +result);
+		scanner.close();
+
+	}
+
+}
